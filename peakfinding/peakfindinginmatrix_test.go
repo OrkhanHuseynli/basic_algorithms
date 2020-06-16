b60a51d286bd7fc4e47513bd12ed5c31076a83a4
OrkhanHuseynli <o.huseynli@gmail.com>
2020-06-16 20:45:51 +0200

    several test cases for peak matrix

--- peakfinding/peakfindinginmatrix_test.go
+++ peakfinding/peakfindinginmatrix_test.go
@@ -6,27 +6,21 @@ import (
 	"testing"
 )
 
-func TestFindPeakInMatrix(t *testing.T) {
-	// Test case 1:
-	input := [][]int{{0,1,2}, {0,2,3}, {0,1,4}}
-	expected :=  []int{2,2}
-	actualRow, actualIndex := FindPeakInMatrix(input)
-	actual := []int{actualRow, actualIndex}
-	assert.Equal(t, expected, actual)
-}
-
-
 var flagtests = []struct {
 	in  [][]int
 	out []int
 }{
-	{ [][]int{{0,1,2}, {0,2,3}, {0,1,4}}, []int{2,2}},
+	{[][]int{{0, 1, 2}}, []int{0, 2}},
+	{[][]int{{3, 1, 0}}, []int{0, 0}},
+	{[][]int{{3, 1, 0},  {0, 4, 3}}, []int{1, 1}},
+	{[][]int{{0, 1, 2}, {0, 2, 3}, {0, 1, 4}}, []int{2, 2}},
 }
-func TestFlagParser(t *testing.T) {
+
+func TestFindPeakInMatrix(t *testing.T) {
 	for i, tt := range flagtests {
-		val := fmt.Sprintf("Test case # %v",i )
+		val := fmt.Sprintf("Test case # %v", i)
 		t.Run(val, func(t *testing.T) {
-			r,i := FindPeakInMatrix(tt.in)
+			r, i := FindPeakInMatrix(tt.in)
 			actual := []int{r, i}
 			assert.Equal(t, tt.out, actual)
 		})
