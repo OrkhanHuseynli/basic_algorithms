15cff6a843f1f86c13d7a403d57479c93d333092
OrkhanHuseynli <o.huseynli@gmail.com>
2020-09-10 13:52:58 +0200

    fixed bug in Insert

--- heap/heap.go
+++ heap/heap.go
@@ -1,5 +1,7 @@
 package heap
 
+import "fmt"
+
 type Heap struct {
 	heaparray []int
 }
@@ -10,48 +12,53 @@ func (h *Heap) size() int {
 
 func (h *Heap) parent(i int) int {
 	if i == 0 {
-		return 0
+		fmt.Println("Parent does not exist")
+		return -1
 	}
-	return i/2
+	return (i-1)/2
 }
 
 func  (h *Heap) left(i int) int {
 	value := 2*i+1
-	if value <= h.size() {
+	if value < h.size() {
 		return value
 	}
-	return 0
+	fmt.Println("Left node does not exist")
+	return -1
 }
 
 func (h *Heap) right(i int) int {
 	value := 2*i+2
-	if value <= h.size() {
+	if value < h.size() {
 		return value
 	}
-	return 0
+	fmt.Println("Right node does not exist")
+	return -1
 }
 
 func (h *Heap) Max() int {
 	if h.size()>0 {
 		return h.heaparray[0]
 	}
-	return 0
+	panic("Heap array is empty")
 }
 
 func (h *Heap) MaxHeapify(i int){
-
 	l:=h.left(i)
 	r:=h.right(i)
 	largest:= i
 
-	if l<=h.size() && h.heaparray[l]>h.heaparray[largest] {
+	if l < h.size() && l >=0 && h.heaparray[l]>h.heaparray[largest] {
 		largest = l
 	}
 
-	if r<=h.size() && h.heaparray[r]>h.heaparray[largest] {
+	if r<h.size() && r >=0 && h.heaparray[r]>h.heaparray[largest] {
 		largest = r
 	}
 
+	if largest == i  {
+		return
+	}
 	swapKeys(h.heaparray, i, largest)
 	h.MaxHeapify(largest)
 }
@@ -65,10 +72,26 @@ func swapKeys(A[]int, i,j int){
 
 func (h *Heap) Insert(key int) {
 	h.heaparray = append(h.heaparray, key)
-	i:=h.size()-1
-	for i  > 0 && h.heaparray[i/2]<key {
-		h.heaparray[i]=h.heaparray[i/2]
-		i = i/2
+	i:= h.size() - 1
+	for i  >= 0 {
+		parent := h.parent(i)
+		if parent >=0 && h.heaparray[parent]<key {
+			h.heaparray[i]=h.heaparray[parent]
+		}else{
+			h.heaparray[i]= key
+			return
+		}
+		i = parent
 	}
-	h.heaparray[i]=key
+}
+
+func (h *Heap) Delete() int {
+	if h.size()>0 {
+		delItem := h.Max()
+		h.heaparray[0] = h.heaparray[h.size()-1]
+		h.heaparray = h.heaparray[:h.size()-2]
+		h.MaxHeapify(0)
+		return delItem
+	}
+	panic("Heap array is empty")
 }
--- heap/heap_test.go
+++ heap/heap_test.go
@@ -34,3 +34,39 @@ func TestSubArray(t *testing.T) {
 		})
 	}
 }
+
+var flagtestsForHeapDelete = []struct {
+	values []int
+	out1 []int
+	//out2 []int
+}{
+	{[]int{5}, []int{5}},
+	{[]int{5,4}, []int{5,4}},
+	{[]int{5,4,3}, []int{5,3,4}},
+
+	//{[]int{4,5}, 5},
+	//{[]int{4,5,3}, 5},
+	//{[]int{9,4,5,3}, 9},
+	//{[]int{1,9,4,5,3}, 9},
+	//{[]int{1,8,4,5,9}, 9},
+	//{[]int{1,7,8,4,0,5}, 8},
+
+
+}
+
+func TestHeap_Delete(t *testing.T) {
+	for i, tt := range flagtestsForHeapDelete {
+		val := fmt.Sprintf("Test case # %v", i)
+		t.Run(val, func(t *testing.T) {
+			l := len(tt.values)
+			heap := Heap{tt.values}
+			sortedArray := make([]int, l)
+			for i:=0; i<l; i++ {
+				sortedArray[i] = heap.Delete()
+			}
+			assert.Equal(t, tt.out1, sortedArray)
+			//fmt.Println(heap.Max())
+			//assert.Equal(t, 0, heap.Max())
+		})
+	}
+}
